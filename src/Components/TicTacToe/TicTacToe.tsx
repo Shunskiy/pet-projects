ed6c98523e9aedda87e0c586b94ee69aa40563fa
Шунский <pas4o@yandex.ru>
2021-09-23 22:34:32 +0400

    Add simple weather realization 😁

--- src/Components/TicTacToe/TicTacToe.tsx
+++ src/Components/TicTacToe/TicTacToe.tsx
@@ -45,7 +45,7 @@ const TicTacToe = () => {
         [0, 1, 2],
         [0, 4, 8],
         [0, 3, 6],
-        [1, 5, 7],
+        [1, 4, 7],
         [2, 4, 6],
         [2, 5, 8],
         [3, 4, 5],
@@ -68,7 +68,6 @@ const TicTacToe = () => {
 
             if (xoField[item[0]] === "O" && xoField[item[1]] === "O" && xoField[item[2]] === "O") {
                 setGameIsOn(false)
-                alert("Победил O")
                 setWinner("O")
             }
         })
--- src/Components/Weather/Weather.tsx
+++ src/Components/Weather/Weather.tsx
@@ -1,19 +1,78 @@
 import {useDispatch} from "react-redux";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {toWeather} from "../../Features/PageSelect/PageSelectSlice";
+import axios from "axios";
 
 const Weather = () => {
+    interface Data {
+        temperature?: string,
+        wind?: string,
+        description?: string
+        forecast?: []
+    }
+
+
     const dispatch = useDispatch()
+
+    const [city, setCity] = useState('moskow')
+    const [data, setData] = useState<Data>({})
+    const [error, setError] = useState('')
+    const url = `https://goweather.herokuapp.com/weather/${city}`
+
+    const updateData = () => {
+        if (city) {
+            axios.get(url).then((response) => {
+                setData(response.data)
+            })
+        }
+
+        if (!city) {
+            alert("Введите город")
+        }
+    }
+
+    const upperFirstChar = (text: string) => {
+        let finalText = text
+        return text.charAt(0).toUpperCase() + finalText.substr(1, finalText.length - 1)
+    }
 
     useEffect(() => {
         document.title = "Pet Project 😺 | Weather"
         dispatch(toWeather())
     }, [dispatch])
 
+    useEffect(() => {
+        updateData()
+    }, [])
+
     return (
-        <h1>
-            Weather ☁
-        </h1>
+        <section className="weather">
+            <h1 className="weather__title">
+                Weather ☁
+            </h1>
+            <h2 className="weather__city">
+                {upperFirstChar(city)}
+            </h2>
+            {!error ? <h2>{error}</h2> : null}
+            <ul className="weather__info">
+                <li>
+                    <b>Temperature:</b> {data.temperature}
+                </li>
+                <li>
+                    <b>Wind:</b> {data.wind}
+                </li>
+                <li>
+                    <b>About:</b> {data.description}
+                </li>
+            </ul>
+            <input value={city} onChange={(e) => {
+                setCity(e.target.value)
+            }}/>
+            <button onClick={() => {
+                updateData()
+            }}>Узнать
+            </button>
+        </section>
     )
 }
 
